bafb95d3219f4b92bb976d93a594c27ded0018e9
fastNick88 <fastNick88@gmail.com>
2018-12-25 01:03:26 +0200

    implement eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const welcomeUser = () => {
-	console.log('Welcome to the Brain Games!');
-	const answer = readlineSync.question('May I have your name?');
-	console.log(`Hello, ${answer}!`);
-}
+const welcomeUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const answer = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${answer}!`);
+};
+
+export default welcomeUser;
