dd42b0a301224661bf04e86f87aef4d37cb8493c
fastNick88 <fastNick88@gmail.com>
2018-12-29 17:02:37 +0200

    fix prime function to care about negative numbers

--- src/games/prime.js
+++ src/games/prime.js
@@ -9,11 +9,10 @@ const upperLimit = 20;
 const lowerLimit = -20;
 
 const isPrime = (number) => {
-  const absNumber = Math.abs(number);
-  for (let i = 2; i <= absNumber / 2; i += 1) {
-    if (absNumber % i === 0) return false;
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) return false;
   }
-  return absNumber !== 1 && absNumber !== 0;
+  return number !== 1 && number !== 0 && number > 0;
 };
 
 const getQuestionAnswerPair = () => {
