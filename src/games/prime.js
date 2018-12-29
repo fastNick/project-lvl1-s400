5f826b8c143047b3b0d5e4e23cb864ade7eacb41
fastNick88 <fastNick88@gmail.com>
2018-12-29 14:26:58 +0200

    support negative numbers while checking isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,4 +1,4 @@
-import { engine } from '..';
+import engine from '..';
 
 import generateRandomNumber from '../utils';
 import { cons } from 'hexlet-pairs';
@@ -6,13 +6,14 @@ import { cons } from 'hexlet-pairs';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const upperLimit = 20;
-const lowerLimit = 0;
+const lowerLimit = -20;
 
 const isPrime = (number) => {
-  for (let i = 2; i < number / 2; i += 1) {
-    if (number % i === 0) return false;
+  const absNumber = Math.abs(number);
+  for (let i = 2; i <= absNumber / 2; i += 1) {
+    if (absNumber % i === 0) return false;
   }
-  return number !== 1 && number !== 0;
+  return absNumber !== 1 && absNumber !== 0;
 };
 
 const getQuestionAnswerPair = () => {
